40fcf5e5c398d8dc1bfe3a93e8e33c2be0a4bc2d
Luan Cardoso <luancardosoti@gmail.com>
2023-06-30 00:06:57 -0300

    feat: Caso de uso de histórico

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -28,6 +28,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInOnSameDate
   }
 
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    const checkIns = this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+
+    return checkIns
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn: CheckIn = {
       id: randomUUID(),
