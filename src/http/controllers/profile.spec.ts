7ff58020ae4e2e8ef57b61778f1bc11825628cd8
Luan Cardoso <luancardosoti@gmail.com>
2023-07-01 14:45:53 -0300

    feat: Teste E2E do perfil

--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,40 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'john.doe@example.com',
+      }),
+    )
+  })
+})
