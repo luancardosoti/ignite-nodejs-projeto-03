f509759c678749118e4906959d12abaf8e784e5c
Luan Cardoso <luancardosoti@gmail.com>
2023-07-01 14:54:21 -0300

    feat: refatoração das rotas e criando controller de criação de academia

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,14 @@
+import { jwtVerify } from '@/http/middlewares/verify-jwt'
+import { FastifyInstance } from 'fastify'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  /** Authenticated */
+
+  app.get('/me', { onRequest: [jwtVerify] }, profile)
+}
