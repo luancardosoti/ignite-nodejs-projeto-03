46d7c157b91af94046ced97664010cf7ff534738
Luan Cardoso <luancardosoti@gmail.com>
2023-07-01 14:59:25 -0300

    feat: criando controller search e nearby da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,16 @@
-import { jwtVerify } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+
+import { jwtVerify } from '@/http/middlewares/verify-jwt'
+
+import { create } from './create'
+import { nearby } from './nearby'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', jwtVerify)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/neary', nearby)
+
+  app.post('/gyms', create)
 }
