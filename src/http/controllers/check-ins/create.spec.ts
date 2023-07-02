f38a0fddc87265c6c75839d83168dd222d787b1d
Luan Cardoso <luancardosoti@gmail.com>
2023-07-02 18:44:39 -0300

    feat: Testes E2E de rotas de check-in

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        latitude: -3.7040974,
+        longitude: -40.3334226,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -3.7040974,
+        longitude: -40.3334226,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
